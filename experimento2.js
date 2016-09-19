e36b36e04ad8400bc981a11b0740f367a6f8615a
Lucas Alves <Lucas Alves>
2016-09-18 23:37:11 -0300

    Improve logic of the experiment 2.

--- experimento2.js
+++ experimento2.js
@@ -8,7 +8,6 @@ module.exports = function(app){
             var seed;
             if(Number.isInteger(currentDay/daysInterval)){
                 seed = gaussian(15, noise);
-                //seed = Math.floor((Math.random() * 10) + 1);
             } else{
                 seed = 0;
             }
@@ -20,7 +19,10 @@ module.exports = function(app){
             var maximum = {};
             var minimum = {};
             var peak = {};
-
+            var percentages = [];
+            var allValues = [];
+            var normalizationFactor = 0;
+            var sortVector = [];
             peak.value = peakValue;
             maximum.value = peak.value + (parseInt(3 * sigma));
             minimum.value = peak.value - (parseInt(3 * sigma));
@@ -28,39 +30,30 @@ module.exports = function(app){
             if(minimum.value < 0 )
                 minimum.value == 0;
             
-            //distributing the percentage roulette
-            var percentages = [];
             for(var i=maximum.value; i>=peak.value; i--){
                 percentages.unshift(Math.pow(i,25));
             }
  
             peak.percentage = maximum.value;
-            var ggEasy = 0;
-            var ggEasy1 = 0;
-            var allValues = [];
-
-
+            
+            var j = 0;
             for(var i = minimum.value; i <= peak.value; i++){
-                var teste = {
+                j++;
+                allValues.push({
                     "value": i,
-                    "percentage": percentages[ggEasy]
-                }
-                ggEasy++;
-                allValues.push(teste)
+                    "percentage": percentages[j]
+                })
             }
             
+            j = 0;
             for(var i = maximum.value; i > peak.value; i--){
-                var teste = {
+                j++;
+                allValues.push({
                     "value": i,
-                    "percentage": percentages[ggEasy1]
-                }
-                ggEasy1++;
-                allValues.push(teste)
+                    "percentage": percentages[j]
+                })
             }
 
-
-            var normalizationFactor = 0;
-            
             for(var pos in allValues){
                 normalizationFactor = normalizationFactor + allValues[pos].percentage;
             }
@@ -71,19 +64,13 @@ module.exports = function(app){
                 allValues[pos].percentage = normalizationFactor * allValues[pos].percentage;
             }
             
-            var sortVector = [];
             for(var pos in allValues){
                 for(var i=0; i<=(allValues[pos].percentage * 100); i++){
                     sortVector.push(allValues[pos].value)
                 }
             }
-            //PRINTAR A PORCENTAGEM E VALOR DA GAUSSIANA
-            //res.json(allValues);
-            
-            
-            //res.json(sortVector);
-            return sortVector[ Math.floor((Math.random() * 100)) ]
 
+            return sortVector[ Math.floor((Math.random() * 100)) ]
         }
 
         var shopping = { buy: [] }
