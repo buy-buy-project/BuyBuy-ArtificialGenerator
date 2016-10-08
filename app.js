5c060979b0fdbacb423518ce3fe2b22f25195366
Lucas Alves <Lucas Alves>
2016-10-08 18:46:44 -0300

    Change port and treating better the mongo connection.

--- app.js
+++ app.js
@@ -2,25 +2,26 @@ var express = require('express')
   , load = require('express-load')
   , moment = require('moment')
   , app  = express()
-  , PORT = 4000
+  , PORT = 8081
   , mongo = require('mongodb').MongoClient;
 
  mongo.connect('mongodb://localhost:27017/buybuy', function(err, db) {
-    if(err) throw err;
+    if(err) 
+      console.log("Error Mongo Connection");
+    else{
+      console.log("Connected correctly to Mongo");
+      global.DATABASE = db; 
+      global.moment = moment;
 
-    console.log("Connected correctly to Mongo");
-    global.DATABASE = db; 
-    global.moment = moment;
+      load('products').
+        then('shopping').
+        then('experimento2').
+        into(app);
 
-    load('products').
-      then('costumers').
-      then('shopping').
-      then('experimento2').
-      into(app);
-
-    app.listen(PORT, function (){
-      console.log("Running Server on " + PORT);
-    });
+      app.listen(PORT, function (){
+        console.log("Running Server on " + PORT);
+      });
+    }
 });
 
 
